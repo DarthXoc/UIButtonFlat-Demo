a722a22167d52548d81bb01149dc29d4a20c5740
Jason Cox <jason@xoc.dev>
2022-01-19 12:46:14 -0600

    Interface Builder Error
    # Bug Fixes
    - When attempting to view the control in Interface Builder, the control would not render

---
+++ UIButtonFlat Demo/Packages.swift
@@ -0,0 +1,15 @@
+//
+//  Packages.swift
+//  UIButtonFlat Demo
+//
+//  Created by Jason Cox on 1/19/22.
+//
+
+import Foundation
+import UIButtonFlat
+
+extension UIButtonFlat {
+    open override class func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+    }
+}
